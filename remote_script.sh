80dc1431196b39a1edb5737adb26186fa13fef5e
Vishal Patil <vishalpatil937@gmail.com>
2019-08-07 09:00:47 +0530

    Update remote_script.sh

--- remote_script.sh
+++ remote_script.sh
@@ -10,26 +10,26 @@
 /usr/local/sbin/oc delete all --selector app=currency-conversion
 
 
-cd /disk2/softwares/openshift_training/Test/docker/eureka-server
+cd /disk2/softwares/openshift_training/docker/eureka-server
 /usr/local/sbin/oc new-app . --name=eureka-server --strategy=docker
 /usr/local/sbin/oc start-build eureka-server --from-dir .
 
-cd /disk2/softwares/openshift_training/Test/docker/Microservice
+cd /disk2/softwares/openshift_training/docker/Microservice
 /usr/local/sbin/oc new-app . --name=microservice --strategy=docker
 /usr/local/sbin/oc start-build microservice --from-dir .
 
-cd /disk2/softwares/openshift_training/Test/docker/microservice2
+cd /disk2/softwares/openshift_training/docker/microservice2
 /usr/local/sbin/oc new-app . --name=microservice2 --strategy=docker
 /usr/local/sbin/oc start-build microservice2 --from-dir .
 
-cd /disk2/softwares/openshift_training/Test/docker/forex-service
+cd /disk2/softwares/openshift_training/docker/forex-service
 /usr/local/sbin/oc new-app . --name=forex-service --strategy=docker
 /usr/local/sbin/oc start-build forex-service --from-dir .
 
-cd /disk2/softwares/openshift_training/Test/docker/api-gateway
+cd /disk2/softwares/openshift_training/docker/api-gateway
 /usr/local/sbin/oc new-app . --name=api-gateway --strategy=docker
 /usr/local/sbin/oc start-build api-gateway --from-dir .
 
-cd /disk2/softwares/openshift_training/Test/docker/currency-conversion
+cd /disk2/softwares/openshift_training/docker/currency-conversion
 /usr/local/sbin/oc new-app . --name=currency-conversion --strategy=docker
 /usr/local/sbin/oc start-build currency-conversion --from-dir .
