abb214980df4d4e365d83b53f688cf237edff1b7
Vishal Patil <vishalpatil937@gmail.com>
2021-06-01 07:25:35 +0530

    Update remote_script.sh

--- remote_script.sh
+++ remote_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/usr/local/sbin/oc login -u system -p admin --server=https://10.20.14.183:8443 --insecure-skip-tls-verify
-/usr/local/sbin/oc project xoriant-cicd
+/usr/local/sbin/oc login -u system -p admin --server=https://10.20.2.217:8443 --insecure-skip-tls-verify
+/usr/local/sbin/oc project devops-training
 
 #/usr/local/sbin/oc delete all --selector app=eureka-server
 /usr/local/sbin/oc delete all --selector app=microservice
@@ -14,15 +14,15 @@
 #/usr/local/sbin/oc new-app . --name=eureka-server --strategy=docker
 #/usr/local/sbin/oc start-build eureka-server --from-dir .
 
-cd /disk2/softwares/openshift_training/docker/Microservice
+cd /home/admin/Microservices-CICD/docker/Microservice
 /usr/local/sbin/oc new-app . --name=microservice --strategy=docker
 /usr/local/sbin/oc start-build microservice --from-dir .
 
-cd /disk2/softwares/openshift_training/docker/microservice2
+cd /home/admin/Microservices-CICD/docker/microservice2
 /usr/local/sbin/oc new-app . --name=microservice2 --strategy=docker
 /usr/local/sbin/oc start-build microservice2 --from-dir .
 
-cd /disk2/softwares/openshift_training/docker/forex-service
+cd /home/admin/Microservices-CICD/docker/forex-service
 /usr/local/sbin/oc new-app . --name=forex-service --strategy=docker
 /usr/local/sbin/oc start-build forex-service --from-dir .
 
@@ -30,6 +30,6 @@ cd /disk2/softwares/openshift_training/docker/forex-service
 #/usr/local/sbin/oc new-app . --name=api-gateway --strategy=docker
 #/usr/local/sbin/oc start-build api-gateway --from-dir .
 
-cd /disk2/softwares/openshift_training/docker/currency-conversion
+cd /home/admin/Microservices-CICD/docker/currency-conversion
 /usr/local/sbin/oc new-app . --name=currency-conversion --strategy=docker
-/usr/local/sbin/oc start-build currency-conversion --from-dir .
+/usr/local/sbin/oc start-build currency-conversion --from-di
