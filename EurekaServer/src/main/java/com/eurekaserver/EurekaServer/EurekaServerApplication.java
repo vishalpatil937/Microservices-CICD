deaacc7b670af90c86e7ea6f9380ad2fcd4a7042
Vishal Patil <vishalpatil937@gmail.com>
2021-06-01 13:19:59 +0530

    Update EurekaServerApplication.java

--- EurekaServer/src/main/java/com/eurekaserver/EurekaServer/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/eurekaserver/EurekaServer/EurekaServerApplication.java
@@ -2,11 +2,18 @@ package com.eurekaserver.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
